93d17225aabd12a22afa76ed55e8ba3268adbc92
Jonathan Alonzo García González <L20290843@cdguzman.tecnm.mx>
2024-09-19 05:05:06 +0000

    validación de caracteres

--- app/models/centro.rb
+++ app/models/centro.rb
@@ -1,4 +1,5 @@
 class Centro < ApplicationRecord
   validates :clave, presence: { message: "no puede ser vacía"}
+  validates :clave, length: { maximum: 10}
   validates :nombre, presence: { message: "no puede ser vacía"}
 end
